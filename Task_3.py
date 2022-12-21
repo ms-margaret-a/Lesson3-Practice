ef2b815d47809420b738950810e64e9e663c387d
ms-margaret-a <puma.9.2@yandex.ru>
2022-12-21 22:17:01 +0300

    lesson 3 practice done

--- Task_3.py
+++ Task_3.py
@@ -1,2 +1,10 @@
 # Реализовать функцию my_func(), которая принимает три  аргумента и возвращает сумму наибольших двух аргументов
 
+def my_func(arg_1, arg_2, arg_3):
+    if arg_1 > arg_3 and arg_2 > arg_3:
+        return arg_1 + arg_2
+    elif arg_1 < arg_3 and arg_2 < arg_1:
+        return arg_1 + arg_3
+    else:
+        return arg_2 + arg_3
+print(f'Результат: {my_func(int(input("Введите первый аргумент ")), int(input("Введите второй аргумент ")), int(input("Введите третий аргумент ")))}')
--- Task_7.py
+++ Task_7.py
@@ -2,3 +2,13 @@
  # Каждое слово состоит из латинских букв в нижнем регистре. Нужно сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы.
  # Используйте написанную ранее функцию int_func().
 
+def int_func (n):
+    sep_words = n.split(' ')
+    total = []
+    for i in sep_words:
+        str_element = str(i)
+        first_letter = str_element[:1].upper()
+        word = first_letter + str_element[1:]
+        total.append(word)
+    return total
+print(int_func("привет андрей"))
